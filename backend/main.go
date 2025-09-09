4ba1230e15a7cdb398e4fb7464f2551bf6cc7a7e
Tymofiy Saukh <130227991+imnottimaq@users.noreply.github.com>
2025-09-09 05:30:57 +0000

    feat: added rate limit to all routes

--- backend/main.go
+++ backend/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
-	"go.mongodb.org/mongo-driver/v2/mongo"
-	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"log"
 	_ "net/http/pprof"
 	"os"
+	"time"
 
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+
+	"github.com/JGLTechnologies/gin-rate-limit"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
@@ -18,30 +21,45 @@ var mongodbCredentials = os.Getenv("MONGO_CREDS")
 var port = os.Getenv("PORT")
 var dbClient, _ = mongo.Connect(options.Client().ApplyURI(mongodbCredentials).SetServerAPIOptions(options.ServerAPI(options.ServerAPIVersion1)))
 
+func keyFunc(c *gin.Context ) string{
+	return c.ClientIP()
+}
+func rateLimitHandler(c *gin.Context, info ratelimit.Info){
+	c.AbortWithStatusJSON(429,gin.H{"error":"too many requests"})
+}
+
 func main() {
 	if err := dbClient.Ping(context.TODO(), readpref.Primary()); err != nil {
 		log.Fatal("Failed to ping MongoDB database")
 	}
 	r := gin.Default()
+	store := ratelimit.InMemoryStore(&ratelimit.InMemoryOptions{
+		Rate: time.Second,
+		Limit: 5,
+	})
+	rateLimiter := ratelimit.RateLimiter(store,&ratelimit.Options{
+		ErrorHandler: rateLimitHandler,
+		KeyFunc: keyFunc,
+	})
 	protected := r.Group("/api/v1")
 	protected.Use(authMiddleware())
 	{
 		//Tasks
-		protected.GET("/tasks", getAllTasks)
-		protected.POST("/tasks", createNewTask)
-		protected.PATCH("/tasks/:taskId", editExistingTask)
-		protected.DELETE("/tasks/:taskId", deleteExistingTask)
+		protected.GET("/tasks", rateLimiter, getAllTasks)
+		protected.POST("/tasks", rateLimiter, createNewTask)
+		protected.PATCH("/tasks/:taskId",rateLimiter, editExistingTask)
+		protected.DELETE("/tasks/:taskId",rateLimiter ,deleteExistingTask)
 
 		//Boards
-		protected.POST("/boards", addBoard)
-		protected.DELETE("/boards/:boardId", deleteBoard)
-		protected.PATCH("/boards/:boardId", editBoard)
+		protected.POST("/boards", rateLimiter, addBoard)
+		protected.DELETE("/boards/:boardId",rateLimiter, deleteBoard)
+		protected.PATCH("/boards/:boardId",rateLimiter, editBoard)
 
 		//Users
-		r.POST("/api/v1/users/create", createUser)
-		r.POST("/api/v1/users/login", loginUser)
-		r.POST("/api/v1/users/refresh", refreshAccessToken)
-		protected.DELETE("/users/delete", deleteUser)
+		r.POST("/api/v1/users/create", rateLimiter, createUser)
+		r.POST("/api/v1/users/login", rateLimiter,loginUser)
+		r.POST("/api/v1/users/refresh",rateLimiter, refreshAccessToken)
+		protected.DELETE("/users/delete",rateLimiter, deleteUser)
 	}
 	if err := r.Run(port); err != nil {
 		log.Fatal("Failed to start server")
