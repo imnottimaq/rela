50e81a6a84c56e508c9e4efa40eca897b190592f
Pomid0rkaaa <121936181+Pomid0rkaaa@users.noreply.github.com>
2025-09-15 20:35:59 +0300

    Revert "Merge branch 'main' of https://github.com/imnottimaq/rela"
    This reverts commit c13ec9a171494adb50c874008523f62d56749681, reversing
    changes made to f72ad39e45633e18da70bc0526916d52fda9c928.

--- backend/handlers.go
+++ backend/handlers.go
@@ -433,6 +433,27 @@ func uploadAvatar(c *gin.Context) {
 
 }
 
+func generateAccessToken(token string, tokenType string) (accessToken string, err error) {
+	parsedToken, _ := jwt.ParseWithClaims(token, &LoginToken{}, func(token *jwt.Token) (any, error) {
+		if token.Method != jwt.SigningMethodHS256 {
+			return nil, fmt.Errorf("unknown signing method: %s", token.Method)
+		}
+		return []byte(pepper), nil
+	})
+	claims := parsedToken.Claims.(*LoginToken)
+	if claims.ExpiresAt < time.Now().UTC().Unix() {
+		return "", fmt.Errorf("expired refresh token")
+	} else {
+		newToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"id":   claims.Id,
+			"exp":  time.Now().UTC().Unix() + 300,
+			"type": tokenType,
+		})
+		accessToken, _ = newToken.SignedString([]byte(pepper))
+		return accessToken, nil
+	}
+}
+
 // @Summary Refresh bearer token
 // @Description For this route, you must have refresh token, that is sent to your browser when you log into user account as an http-only cookie
 // @Router /api/v1/users/refresh [get]
@@ -574,4 +595,17 @@ func editBoard(c *gin.Context) {
 			c.AbortWithStatusJSON(500, gin.H{"error": "Failed to insert board"})
 		}
 	}
+}
+
+func validatePassword(password string) bool {
+	if len(password) < 8 {
+		return false
+	}
+
+	hasLower := regexp.MustCompile(`[a-z]`).MatchString(password)
+	hasUpper := regexp.MustCompile(`[A-Z]`).MatchString(password)
+	hasDigit := regexp.MustCompile(`\d`).MatchString(password)
+	hasSpecial := regexp.MustCompile(`[!"#$%&'()*+,\-./:;<=>?@[\\\]^_{|}~]`).MatchString(password)
+
+	return hasLower && hasUpper && hasDigit && hasSpecial
 }
